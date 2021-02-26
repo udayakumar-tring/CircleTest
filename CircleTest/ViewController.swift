b2bb86536c15dca619a7922a1cf4c32574ec156a
udayakumar-tring <udaya.k@tringapps.com>
2021-02-26 16:25:37 +0530

    Initial Commit

---
+++ CircleTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleTest
+//
+//  Created by tringapps on 26/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Did load Called")
+    }
+
+
+}
+
